1ed616744e2b3f27e421c7ce198d128bd070d830
freakyInsane <vlad.petrishin@gmail.com>
2017-08-24 16:11:11 +0300

    Rewriting initial code using jsforce library

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('qwe');
+/**
+ * Module dependencies.
+ */
+var express        = require( 'express' );
+var http           = require( 'http' );
+var jsforce        = require('jsforce');
+
+var app            = express();
+app.set( 'port', process.env.PORT || 3001 );
+app.get('/', function (req, res) {
+  var conn = new jsforce.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    // loginUrl : 'https://test.salesforce.com'
+  });
+  var username = 'vlad.petrishin1@softheme.com';
+  var password = '123456asnaebtfFW3AxD6TNrQ94IFl5epgE8';
+  conn.login(username, password, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+    res.send('JSForce Connect Successed!');
+  });
+  //res.send('Hello World');
+});
+
+http.createServer( app ).listen( app.get( 'port' ), function (){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
+});
