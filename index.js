9b973cbc4896c84a8220b8f3ef7e8f5cf0d0ca21
freakyInsane <vlad.petrishin@gmail.com>
2017-08-24 17:29:09 +0300

    Adding function to select number of org accounts

--- index.js
+++ index.js
@@ -4,30 +4,39 @@
 var express        = require( 'express' );
 var http           = require( 'http' );
 var jsforce        = require('jsforce');
+var credentials	   = require('./credentials.js');
+var conn;
 
 var app            = express();
 app.set( 'port', process.env.PORT || 3001 );
 app.get('/', function (req, res) {
-  var conn = new jsforce.Connection({
+  conn = new jsforce.Connection({
     // you can change loginUrl to connect to sandbox or prerelease env.
     // loginUrl : 'https://test.salesforce.com'
   });
-  var username = 'vlad.petrishin1@softheme.com';
-  var password = '123456asnaebtfFW3AxD6TNrQ94IFl5epgE8';
+  var username = credentials.getUsername();
+  var password = credentials.getPassword();
   conn.login(username, password, function(err, userInfo) {
     if (err) { return console.error(err); }
-    // Now you can get the access token and instance URL information.
-    // Save them to establish connection next time.
-    console.log(conn.accessToken);
-    console.log(conn.instanceUrl);
-    // logged in user property
-    console.log("User ID: " + userInfo.id);
-    console.log("Org ID: " + userInfo.organizationId);
-    // ...
+    //console.log(conn.accessToken);
+    //console.log(conn.instanceUrl);
+    //console.log("User ID: " + userInfo.id);
+    //console.log("Org ID: " + userInfo.organizationId);
     res.send('JSForce Connect Successed!');
   });
-  //res.send('Hello World');
 });
+
+
+app.get('/accs', function (req, res) {
+  var records = [];
+  conn.query("SELECT Id, Name FROM Account", function(err, result) {
+    if (err) { return console.error(err); }
+    console.log("total : " + result.totalSize);
+    console.log("fetched : " + result.records.length);
+  });
+
+});
+
 
 http.createServer( app ).listen( app.get( 'port' ), function (){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
